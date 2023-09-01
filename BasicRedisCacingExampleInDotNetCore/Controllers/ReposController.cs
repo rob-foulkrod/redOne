609b26c420b0a9b7878f0bd3b7251ef79410e795
Rob Foulkrod <89028299+rob-foulkrod@users.noreply.github.com>
2023-09-01 14:54:00 -0400

    Redis is back baby!

--- BasicRedisCacingExampleInDotNetCore/Controllers/ReposController.cs
+++ BasicRedisCacingExampleInDotNetCore/Controllers/ReposController.cs
@@ -42,34 +42,34 @@ namespace BasicRedisCacingExampleInDotNetCore.Controllers
             RedisValue cache;
 
 
-            // var success = false;
+            var success = false;
             var startTime = DateTime.UtcNow;
             var timer = System.Diagnostics.Stopwatch.StartNew();
-            // try
-            // {
-            //     cache = await db.StringGetAsync($"repos:{username}");
-            //     success = true;
-            // }
-            // catch (Exception ex)
-            // {
-            //     _telemetry.TrackException(ex);
-            //     throw new Exception("Redis Exception", ex);
-            // }
-            // finally
-            // {
+            try
+            {
+                cache = await db.StringGetAsync($"repos:{username}");
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                _telemetry.TrackException(ex);
+                throw new Exception("Redis Exception", ex);
+            }
+            finally
+            {
                 timer.Stop();
-            //     _telemetry.TrackDependency("RedisCache", "Example Redis Cache", "CacheLookup", startTime, timer.Elapsed, success);
-            // }
+                _telemetry.TrackDependency("RedisCache", "Example Redis Cache", "CacheLookup", startTime, timer.Elapsed, success);
+            }
 
-            // if (string.IsNullOrEmpty(cache))
-            // {
+            if (string.IsNullOrEmpty(cache))
+            {
                 //Since we haven't seen this username recently, let's grab it from the github API
                 var gitData = await _client.GetFromJsonAsync<GitResponseModel>($"users/{username}");
                 var data = new ResponseModel { Repos = gitData.PublicRepos.ToString(), Username = username, Cached = true };
                 await db.StringSetAsync($"repos:{username}", JsonSerializer.Serialize(data), expiry: TimeSpan.FromMinutes(5));
                 data.Cached = false;
                 cache = JsonSerializer.Serialize(data);
-            // }
+            }
 
             funcTimer.Stop();
             TimeSpan timeTaken = funcTimer.Elapsed;
